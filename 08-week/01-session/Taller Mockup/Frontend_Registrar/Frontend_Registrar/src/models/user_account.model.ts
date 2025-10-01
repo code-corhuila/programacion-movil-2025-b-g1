8acea4a7ee474a256512f91b8cee6c481bd02c9d
MarianaPereira04 <danaypospina123@gmail.com>
2025-09-30 22:28:19 -0500

    chore: update Taller Mockup and add Frontend_Registrar

---
+++ 08-week/01-session/Taller Mockup/Frontend_Registrar/Frontend_Registrar/src/models/user_account.model.ts
@@ -0,0 +1,9 @@
+export interface User_Account {
+    names: string;
+    last_names: string;
+    email: string;
+    phone: number;
+    username: string;
+    password_hash: string;
+    role: string;
+}
