18cc4554f331255f8b062a88dbff0338f0843c68
MarianaPereira04 <danaypospina123@gmail.com>
2025-10-10 17:25:24 -0500

    docs: add Taller de Mockup

---
+++ 08-week/01-session/Taller Mockup/Frontend_Registrar/Frontend_Registrar/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Frontend_Registrar',
+  webDir: 'dist'
+};
+
+export default config;
